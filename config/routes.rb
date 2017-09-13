f8868b81ab6005022b3a52d3ef5e870776756148
adayNU <aday.nu@gmail.com>
2017-09-12 20:30:20 -0700

    added routing to support any wildcard route for cache busting

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'index#show'
 
+   get '*path', to: 'index#show'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
