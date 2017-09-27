58e4d0aa0de8fd6e77e78949b988c56e7a7015cb
aday <aday@liveramp.com>
2017-09-27 11:34:48 -0400

    added check for user agent to return mp4 for imessage

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,12 +2,16 @@ class IndexController < ApplicationController
   BASE_URL = "http://api.giphy.com"
   ENDPOINT = "v1/gifs/random"
   GIF_TAG = "disappointed"
+  USER_AGENT_REGEX = /(Twitterbot(.*)facebookexternalhit)|(facebookexternalhit(.*)Twitterbot)/
 
   def show
+    # if iMessage return mp4 instead of gif
+    link_type = (USER_AGENT_REGEX.match(request.user_agent) ? "image_mp4_url" : "image_url")
+
     url = "#{BASE_URL}/#{ENDPOINT}?tag=#{GIF_TAG}&api_key=#{API_KEYS["giphy"]["key"]}"
     result = RestClient.get(url)
 
     json = JSON.parse(result.body)
-    redirect_to json["data"]["image_url"]
+    redirect_to json["data"][link_type]
   end
 end
